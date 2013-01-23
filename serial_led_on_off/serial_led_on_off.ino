131ed5a0873e5500fb3b601ec9e2b41faa097468
fidepus <mail@fidepus.de>
2013-01-23 23:31:57 +0100

    Yeah, Mr Whit, switch cases, bitch

--- serial_led_on_off/serial_led_on_off.ino
+++ serial_led_on_off/serial_led_on_off.ino
@@ -1,1 +1,39 @@
+/*
+  Switch an attached LED on or off via serial console.
+  
+  Circuit:
+  * LED: pin 13 + ground
+  
+  created 23.01.2013
+  by fidepus
+*/
 
+// pin for the LED:
+const int led = 13;
+
+void setup() {
+  //initialize serial:
+  Serial.begin(9600);
+  // make the pin an output:
+  pinMode(led, OUTPUT);
+  
+}
+
+void loop() {
+  
+  // read any available serial:
+  if (Serial.available() > 0) {
+    int inByte = Serial.read();
+    // we will use switch cases to turn the led on or off
+    switch (inByte) {
+      // a 1 on the serial console will switch the led on
+    case '1':    
+      digitalWrite(led, HIGH);
+      break;
+      // everything else will switch it off
+    default:
+      digitalWrite(led, LOW);
+    }
+  }
+}
+    
